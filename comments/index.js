4795ebc4c9f40649b7d0064c672fd2c9c20b5a55
Noor Ahmad <n.ahmad0093@gmail.com>
2023-03-10 16:49:00 +0300

    updates

---
+++ comments/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+
+const app = express()
+const PORT = 4001
+
+app.use(express.json())
+
+const commentsByPostID = {}
+
+app.get('/posts/:id/comments', (req, res) => {
+    const { id } = req.params
+    res.status(200).send(commentsByPostID[id] || [])
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentID = randomBytes(4).toString('hex')
+    const { content } = req.body
+    const { id } = req.params
+    const comments = commentsByPostID[id] || []
+    comments.push({ id: commentID, content })
+    commentsByPostID[id] = comments
+
+    res.status(201).send(comments)
+})
+
+app.listen(PORT, () => {
+    console.log("Listening on PORT:", PORT);
+})
---
+++ posts/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const { randomBytes } = require('crypto')
+
+const app = express()
+app.use(express.json())
+
+const PORT = 4000
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+
+})
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+    posts[id] = { id, title }
+    res.status(201).send(posts[id])
+})
+
+app.listen(PORT, () => {
+    console.log("Listening on PORT:", PORT);
+})
+
